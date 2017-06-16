ec1f8d3ea06a095362b02ad996359e7ee082feaf
Alfredo <gopher.ae@gmail.com>
2017-06-16 00:16:45 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    nickNames = [];
+
+var PORT = '8080';
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+server.listen(PORT);
+console.log(' [] Magic on address:',PORT);
+
+io.sockets.on('connection',function(socket){
+    socket.on('new_user',function(data, callback){
+        if (nickNames.indexOf(data.nickName) != -1) {
+            callback({isValid: false});
+        } else {
+            socket.nickname = data.nickName;
+            nickNames.push(data.nickName);
+            io.sockets.emit('nicknames',nickNames);
+            callback({isValid:true, users:nickNames});
+            // io.sockets.emit('new message',{msg:data.msg, user:data.user});
+        }
+    });
+
+    socket.on('send message',function(data){
+        io.sockets.emit('new message',{msg:data.msg, user:data.user});
+    });
+});
